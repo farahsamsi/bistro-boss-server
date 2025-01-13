ef7c637324122d3ebdf0af113ff2e4aeeb306a5d
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-01-13 12:25:22 +0600

    Create a JWT token and save it on local storage

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const app = express();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tu4i6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,7 +31,36 @@ async function run() {
     const reviewCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
 
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // verify token middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers);
+
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Forbidden Access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      // if (!token) {
+
+      // }
+
+      // next();
+    };
+
     // ______ USERS related APIs__________
+    app.get("/users", verifyToken, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
 
@@ -46,11 +76,6 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
-      const result = await userCollection.find().toArray();
-      res.send(result);
-    });
-
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -60,7 +85,7 @@ async function run() {
         },
       };
       const result = await userCollection.updateOne(filter, updatedDoc);
-      res.send();
+      res.send(result);
     });
 
     app.delete("/users/:id", async (req, res) => {
